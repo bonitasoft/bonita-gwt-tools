48d2543b98560c6ec740203babe356b3ce91afa2
Colin Puy <colin.puy@bonitasoft.com>
2013-05-21 12:31:54 +0000

    add comments

--- src/main/java/org/bonitasoft/tools/gwt/BonitaJettyLauncher.java
+++ src/main/java/org/bonitasoft/tools/gwt/BonitaJettyLauncher.java
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,21 +25,24 @@ import org.mortbay.xml.XmlConfiguration;
 
 import com.google.gwt.core.ext.TreeLogger;
 
-
 /**
+ * Custom jetty launcher for Bonita GWT debug mode
+ * 
+ * - Enable JNDI ENC
+ * - Load jetty.xml
+ * 
  * @author Colin PUY
- *
  */
 public class BonitaJettyLauncher extends JettyLauncher {
 
     /** get the JNDI ENC (ie: JNDI URLs starting with java:comp/) configured in Jetty */
     private final static String[] WEBAPPCONTEXT_CONFIGURATION_CLASSES = {
-            "org.mortbay.jetty.webapp.WebInfConfiguration", 
+            "org.mortbay.jetty.webapp.WebInfConfiguration",
             "org.mortbay.jetty.webapp.JettyWebXmlConfiguration",
             "org.mortbay.jetty.plus.webapp.EnvConfiguration",
-            "org.mortbay.jetty.plus.webapp.Configuration", 
+            "org.mortbay.jetty.plus.webapp.Configuration",
     };
-    
+
     @Override
     protected WebAppContext createWebAppContext(TreeLogger logger, File appRootDir) {
         WebAppContext webAppContext = super.createWebAppContext(logger, appRootDir);
